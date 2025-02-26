8d67f8190d4c4fa3019b3a0207801879d1016d54
abdulrahman <abdulrahmn398@outlook.com>
2025-02-26 15:09:03 +0200

    Add Memento project with TextEditor implementation
    This commit introduces a new "Memento" project to the
    DesignPatterns solution, including its configuration for
    Debug and Release modes. A new `Memento.csproj` file
    specifies the project SDK and settings.
    
    Additionally, a `Program` class demonstrates the
    functionality of a new `TextEditor` class, which
    implements the Memento design pattern. The `TextEditor`
    class allows for content modification with undo and redo
    capabilities, utilizing stacks to manage previous and
    next states.

---
+++ Memento/Program.cs
@@ -0,0 +1,22 @@
+﻿using Memento;
+
+class Program 
+{
+    static void Main()
+    {
+        TextEditor textEditor = new TextEditor();
+
+        textEditor.Content = "Hello, ";
+        textEditor.Save();
+
+        textEditor.Content = "World!";
+        textEditor.Save();
+
+        textEditor.Content = "Welcome to Design Pattern Course!";
+        textEditor.Undo();
+        textEditor.Undo();
+        textEditor.Redo();
+
+        Console.WriteLine(textEditor.Content);
+    }
+}
---
+++ Memento/TextEditor.cs
@@ -0,0 +1,46 @@
+﻿namespace Memento;
+
+class TextEditor
+{
+    public string Content { get; set; }
+    private readonly Stack<string> _prevState;
+    private readonly Stack<string> _nextState;
+
+    public TextEditor()
+    {
+        Content = "";
+        _prevState = [];
+        _nextState = [];
+    }
+
+    public void Save()
+    {
+        _prevState.Push(Content);
+        _nextState.Clear();
+    }
+
+    public string Undo()
+    {
+        if (_prevState is not null)
+        {
+            _nextState.Push(Content);
+            Content = _prevState.Pop();
+
+            return Content;
+        }
+
+        return null!;
+    }
+
+    public string Redo()
+    {
+        if (_nextState is not null)
+        {
+            _prevState.Push(Content);
+            Content = _nextState.Pop();
+            return Content;
+        }
+
+        return null!;
+    }
+}
